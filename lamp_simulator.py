c1ce1f5410b2f26b4c891a877764008c69b012c1
Enrico <enrizangri@gmail.com>
2023-03-10 17:02:09 +0100

    Added ip address and supabase
    Now the controller can take all the data from supabase
    
    The controller will now receive the API post request and change the light with the correct ip address
    
    Getter API will now return a json with data about the last lamp that has been modified

--- lamp_simulator.py
+++ lamp_simulator.py
@@ -1,8 +1,20 @@
 from flask import Flask, jsonify, render_template, request
+from supabase import create_client, Client
 import paho.mqtt.client as mqtt
 import json
 
+url: str = "https://banraxrzqacvpzsonavh.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJhbnJheHJ6cWFjdnB6c29uYXZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NzczMzkzNTksImV4cCI6MTk5MjkxNTM1OX0.gRfvdnzSN_jBTmi2iY1GPOK10flQIp_tUgyeRD3K90I"
+supabase: Client = create_client(url, key)
+
 app = Flask(__name__)
+
+lights = (supabase.table("light").select("*").execute()).data
+lights_ip = {}
+for light in lights:
+    lights_ip[light["ip_address"]] = light
+
+last_changed = lights_ip[list(lights_ip.keys())[0]]
 
 # Identificativo del lampione
 lamp_id = 123
@@ -46,19 +58,23 @@ mqtt_client.on_message = on_message
 # API REST per ottenere lo stato del lampione e la luminosità impostata
 @app.route('/lamp', methods=['GET'])
 def get_lamp_status():
-    return jsonify({'lamp_id': lamp_id, 'lamp_status': lamp_status, 'brightness': brightness})
+    return jsonify({'lamp_id': last_changed["id"], 'lamp_status': last_changed["state"], 'brightness': last_changed["brightness"], 'lamp_ip': last_changed["ip_address"]})
 
 # API REST per accendere o spegnere il lampione e impostare la luminosità
 @app.route('/lamp', methods=['POST'])
 def set_lamp_status():
-    global lamp_status, brightness
+    global lamp_status, brightness, last_changed
     data = request.get_json()
-    if (data['lamp_id'] != lamp_id):
-        return jsonify({'lamp_id': lamp_id, 'error': 'lamp_id is not the same of this lamp.'}), 400
+    """ if (data['lamp_id'] != lamp_id):
+        return jsonify({'lamp_id': lamp_id, 'error': 'lamp_id is not the same of this lamp.'}), 400 """
+    if('lamp_ip' not in data):
+        return jsonify({'error': 'The ip address of the lamp is not defined'}), 400
     if('lamp_status' in data):
-        lamp_status = data['lamp_status']
+        lights_ip[data['lamp_ip']]["state"] = data['lamp_status']
+        last_changed = lights_ip[data['lamp_ip']]
     if('brightness' in data):
-        brightness = data['brightness']
+        lights_ip[data['lamp_ip']]["brightness"] = data['brightness']
+        last_changed = lights_ip[data['lamp_ip']]
     return jsonify({'lamp_id': lamp_id, 'lamp_status': lamp_status, 'brightness': brightness})
 
 # Pagina web che mostra il quadrato sincronizzato con lo stato del lampione e la luminosità impostata
